ce4504b42160deefc8cff8f9bbff77b22f8670f7
Júlia Vasconcelos Fernandino <146314384+jufernandino@users.noreply.github.com>
2023-11-04 01:01:40 -0300

    Adicionando arquivos iniciais

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import './App.css'
+
+function Playlist(){
+
+  //ARRAY COM LISTA DAS MUSICAS
+  const [musics, setMusics] = useState([
+    {id:1,
+      title: "The Zephyr Song",
+      artist: "Red Hot Chili Pepers",
+      album: "By The Way",
+      favorite: false,},
+    {
+      id:2,
+      title: "Talk",
+      artist: "Coldplay",
+      album: "X&Y",
+      favorite: false,
+    },
+    {
+      id:3,
+      title: "Cidade Negra",
+      artist: "Firmamento",
+      album: "Cidade Negra AcÚstico MTV",
+      favorite: false,
+    },
+  ]);
+  
+  return ( 
+    //APLICATIVO GERAL
+    <div className="app"> 
+
+      {/*AREA EM QUE APARECE AS MUSICAS*/}
+      <div className="playlist"> 
+        {musics.map ((music) => ( // Mapear os itens da função "musics"
+          <ul key={music.id}>  {/*Listar todas as músicas*/}
+            <div className="music">
+              
+              {/*AREA #TITULO*/}
+              <div className="TITLE">
+                <p>{music.title}</p>
+                <p>{music.artist}</p>
+              </div>
+
+              {/*AREA ALBUM*/}
+              <div className="ALBUM">
+                <p>({music.album})</p>
+              </div>
+
+              {/*AREA COM INTERAÇÕES*/}
+              <div className="ITERACTIONS">
+                <button className='favorite-button'>
+                  <span className="material-symbols-outlined"> favorite </span>
+                </button>
+                <button className='delete-button'>
+                <span className="material-symbols-outlined"> delete </span>
+                </button>
+              </div>
+
+            </div>
+          </ul> //Fim da listagem das músicas
+        ))} {/*Fim do map*/}
+      
+      </div> {/*FIM DA AREA DAS MUSICAS*/}
+
+    </div>
+    //FIM DO APLICATIVO GERAL
+  );
+}
+
+function App(){
+  
+  return ( 
+      Playlist()    
+    )
+}
+
+export default App
